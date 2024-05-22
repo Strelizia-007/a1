8697e8f1bbb10a4982c22e7d60a5a9f93f579a9e
Strelizia-007 <143823798+Strelizia-007@users.noreply.github.com>
2024-05-22 14:55:05 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -2,17 +2,17 @@ from os import path, getenv
 import os
 
 class Config:
-    API_ID = int(getenv("API_ID", "0112234"))
-    API_HASH = getenv("API_HASH", "abcdefg")
-    BOT_TOKEN = getenv("BOT_TOKEN", "1234567891:rkndeveloperDEhdhyjjvjjftSEW")
-    UPDATE_CHANNEL = getenv("UPDATE_CHANNEL", "RknDeveloper")
-    UPDATECHANNEL_ID = int(getenv("UPDATECHANNEL_ID", "-1001819787652"))
-    ADMIN = list(map(int, getenv("ADMIN", "6151758586").split()))
-    MONGO_URI = getenv("MONGO_URI", "")
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002120760645"))
+    API_ID = int(getenv("API_ID", "29922662"))
+    API_HASH = getenv("API_HASH", "fabd9f89368de7cc31357522a0089a56")
+    BOT_TOKEN = getenv("BOT_TOKEN", "6410417594:AAG4GmfMx8UkbgCxypy24OX_Ht-90CR75sc")
+    UPDATE_CHANNEL = getenv("UPDATE_CHANNEL", "CV_Offical")
+    UPDATECHANNEL_ID = int(getenv("UPDATECHANNEL_ID", "-1001574664407"))
+    ADMIN = list(map(int, getenv("ADMIN", "6047510747").split()))
+    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://ironman:ironman@cluster0.o1yjwis.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002116542152"))
     
     PORT = os.environ.get("PORT", "8080")
     RKN_PIC = os.environ.get("RKN_PIC", "https://graph.org/file/e846f9375e9d4f4975ce4.jpg")
-    BOT_USERNAME = os.environ.get("BOT_USERNAME","Rkn_AutoRequestApprovebot")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME","TheStreliziaBot")
     
 rkn1 = Config()
