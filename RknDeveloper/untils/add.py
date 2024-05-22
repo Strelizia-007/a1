65c60c50a27c9ce602b7bf5ef50b624db82f9d14
Strelizia-007 <143823798+Strelizia-007@users.noreply.github.com>
2024-05-22 17:16:04 +0530

    Update add.py

--- RknDeveloper/untils/add.py
+++ RknDeveloper/untils/add.py
@@ -7,9 +7,9 @@ from pyrogram.types import BotCommand
 async def set_commands(app):
     RKN = [
         BotCommand("start", "Check Bot Alive."),
-        BotCommand("rknusers", "Total Users Check.(Aᴅᴍɪɴ Oɴʟʏ)"),
-        BotCommand("broadcast", "Broadcast Massage Send All Users In Bot (Aᴅᴍɪɴ Oɴʟʏ)."),
-        BotCommand("fd_broadcast", "Broadcast massage Forward Not Remove (Aᴅᴍɪɴ Oɴʟʏ)."),
+        BotCommand("users", "Total Users Check.(Aᴅᴍɪɴ Oɴʟʏ)"),
+        BotCommand("broadcast", "Broadcast Message Send All Users In Bot (Aᴅᴍɪɴ Oɴʟʏ)."),
+        BotCommand("fd_broadcast", "Broadcast Message with Forward Tag (Aᴅᴍɪɴ Oɴʟʏ)."),
         BotCommand("restart", "Restarts or re-deploys the server (Aᴅᴍɪɴ Oɴʟʏ).")
  
     ]
